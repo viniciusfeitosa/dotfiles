87ece53941a07a1074b08125ca6b205e78ae23b8
Vinicius Feitosa Pacheco <vfpweb@gmail.com>
2016-12-27 22:51:00 -0300

    Define wombat as theme

--- emacs.d/elisp/base-theme.el
+++ emacs.d/elisp/base-theme.el
@@ -1,6 +1,7 @@
-(use-package zenburn-theme
-  :defer t
-  :init
-  (load-theme 'zenburn t))
+;; (use-package zenburn-theme
+;;   :defer t
+;;   :init
+;;   (load-theme 'zenburn t))
+(load-theme 'wombat t)
 
 (provide 'base-theme)
