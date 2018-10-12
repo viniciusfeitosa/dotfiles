066092997507d281c96bb8f6a6318e6ff9df4cdc
Vinicius Feitosa Pacheco <vfpweb@gmail.com>
2018-10-12 16:35:22 -0300

    Update base-teme to monochromatic

--- emacs.d/elisp/base-theme.el
+++ emacs.d/elisp/base-theme.el
@@ -2,6 +2,11 @@
 ;;   :defer t
 ;;   :init
 ;;   (load-theme 'zenburn t))
-(load-theme 'adwaita t)
+;; (load-theme 'adwaita t)
+
+(use-package eziam-common
+   :defer t
+   :init
+   (load-theme 'eziam-light t))
 
 (provide 'base-theme)
